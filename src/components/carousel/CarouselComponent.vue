cc295a5ff74ebeebd57304d9ce658a216f1450dd
caioliveira277 <caio-henrique718@outlook.com>
2022-04-06 15:10:01 -0300

    adding carousel component

---
+++ src/components/carousel/CarouselComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="carousel row row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 overflow-auto px-4 px-sm-0"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<style src="./styles.scss"></style>
