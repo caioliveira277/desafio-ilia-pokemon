f320a13ab8236b212dfa6509c798dad1c0a35893
caioliveira277 <caio-henrique718@outlook.com>
2022-04-06 17:39:43 -0300

    adding link to home in header logo

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -1,20 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
 
 <template>
   <header class="header shadow-lg bg-dark py-3">
     <div
       class="container d-flex align-items-center flex-column justify-content-between"
     >
-      <picture>
-        <source src="@/assets/images/pokemon-logo.webp" type="image/webp" />
-        <img
-          src="@/assets/images/pokemon-logo.png"
-          width="390"
-          height="147"
-          alt="Logo Pokémon"
-          class="w-100 h-100"
-        />
-      </picture>
+      <RouterLink to="/">
+        <picture>
+          <source src="@/assets/images/pokemon-logo.webp" type="image/webp" />
+          <img
+            src="@/assets/images/pokemon-logo.png"
+            width="390"
+            height="147"
+            alt="Logo Pokémon"
+            class="w-100 h-100"
+          />
+        </picture>
+      </RouterLink>
       <fieldset class="mt-5 text-white">
         <h1 for="input-search" class="mb-1 h4">Buscar pokémons:</h1>
         <div class="input-group">
