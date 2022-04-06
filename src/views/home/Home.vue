55b3c10d17386f3095bf6faa9f7247b7e7bd53ad
caioliveira277 <caio-henrique718@outlook.com>
2022-04-06 11:30:55 -0300

    changing home view

---
+++ src/views/home/Home.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Card } from "@/components";
+</script>
+
+<template>
+  <main class="container pt-5">
+    <div class="row">
+      <Card
+        class="col-3"
+        image-url="https://images.pokemontcg.io/xy1/1.png"
+        id="xy1-1"
+        title="Venusaur-EX"
+        :list="['Grass']"
+      />
+      <Card
+        class="col-3"
+        image-url="https://images.pokemontcg.io/xy1/1.png"
+        id="xy1-1"
+        title="Venusaur-EX"
+        :list="['Grass']"
+      />
+      <Card
+        class="col-3"
+        image-url="https://images.pokemontcg.io/xy1/1.png"
+        id="xy1-1"
+        title="Venusaur-EX"
+        :list="['Grass']"
+      />
+      <Card
+        class="col-3"
+        image-url="https://images.pokemontcg.io/xy1/1.png"
+        id="xy1-1"
+        title="Venusaur-EX"
+        :list="['Grass']"
+      />
+    </div>
+  </main>
+</template>
