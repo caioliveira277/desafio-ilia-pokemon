d8c2c46d60d49587ccf8da928eea5b9f85c8e41c
caioliveira277 <caio-henrique718@outlook.com>
2022-04-06 17:37:32 -0300

    adding more details view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/home/HomeView.vue";
+import HomeView from "@/views/home/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +8,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/mais-detalhes",
+      name: "moreDetails",
+      component: () => import("@/views/more-details/MoreDetailsView.vue"),
     },
   ],
 });
---
+++ src/views/more-details/MoreDetailsView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="container py-5">detail</main>
+</template>
