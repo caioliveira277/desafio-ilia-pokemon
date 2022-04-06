72c6c05192003afdd1a40e626ccb92b030acf7dc
caioliveira277 <caio-henrique718@outlook.com>
2022-04-06 11:30:39 -0300

    calling global styles in the app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import { Header } from "@/components";
 </script>
 
 <template>
+  <Header />
   <RouterView />
 </template>
+
+<style src="@/theme/global.scss"></style>
