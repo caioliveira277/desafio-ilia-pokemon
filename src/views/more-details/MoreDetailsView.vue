f85b1d4969f88a26f4acf9ff2a7d73d7aa1a5655
caioliveira277 <caio-henrique718@outlook.com>
2022-04-07 18:06:57 -0300

    starting view on route for more details

--- src/views/more-details/MoreDetailsView.vue
+++ src/views/more-details/MoreDetailsView.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useStore } from "@/context/store";
+
+const store = useStore();
+const currentPokemon = store.state.currentPokemon;
+
+console.log(store);
+</script>
 
 <template>
-  <main class="container py-5">detail</main>
+  <main class="container py-5 justify-content-center align-items-center d-flex">
+    <img
+      :src="currentPokemon.images.large"
+      :alt="`Imagem do pokemon ${currentPokemon.name}`"
+    />
+  </main>
 </template>
+
+<style src="./styles.scss" scoped></style>
