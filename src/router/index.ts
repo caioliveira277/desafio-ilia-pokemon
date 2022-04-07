ee0624f7cafd6d1330558163f8ef2247f08edf02
caioliveira277 <caio-henrique718@outlook.com>
2022-04-07 18:05:01 -0300

    adding splash view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/home/HomeView.vue";
+import MoreDetailsView from "@/views/more-details/MoreDetailsView.vue";
+import SpashView from "@/views/splash/splashView.vue";
+import { HeaderComponent } from "@/components";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "spash",
+      component: SpashView,
+    },
+    {
+      path: "/inicio",
+      name: "initial",
+      components: {
+        default: HomeView,
+        HeaderComponent,
+      },
     },
     {
       path: "/mais-detalhes",
       name: "moreDetails",
-      component: () => import("@/views/more-details/MoreDetailsView.vue"),
+      components: {
+        default: MoreDetailsView,
+        HeaderComponent,
+      },
     },
   ],
 });
---
+++ src/views/splash/splashView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+function handleClick(): void {
+  router.push({
+    name: "initial",
+  });
+}
+</script>
+
+<template>
+  <main class="p-3">
+    <div
+      class="container align-items-center d-flex flex-column justify-content-center h-100"
+    >
+      <img
+        src="@/assets/icons/hover-symbol.svg"
+        alt="Icone de representação do cartão"
+        class="mb-5"
+      />
+      <h1 class="text-white fw-bold">
+        Olá, para interagir com os "cards" é necessário passar o cursor do mouse
+        ou clicar ✌
+      </h1>
+      <button type="button" @click="handleClick" class="btn btn-light mt-5">
+        Ok!!
+      </button>
+    </div>
+  </main>
+</template>
+
+<style src="./styles.scss" scoped></style>
