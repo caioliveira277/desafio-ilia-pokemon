c3069549230645725b796465e21c6904f42d6a5b
caioliveira277 <caio-henrique718@outlook.com>
2022-04-10 18:14:12 -0300

    adding animation

--- src/components/modal/ModalComponent.vue
+++ src/components/modal/ModalComponent.vue
@@ -1,31 +1,43 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useStore } from "@/context/store";
 
 const store = useStore();
-const modal = store.state.modal;
+const modal = computed(() => store.getters.modal);
+
+function closeModal() {
+  store.dispatch("toggleModalState");
+}
 </script>
 <template>
-  <teleport to="body">
+  <Teleport to="body">
     <div
-      class="modal position-fixed top-0 d-flex align-items-center justify-content-center p-2"
+      class="modal position-fixed top-0 align-items-baseline justify-content-center p-2"
       tabindex="-1"
-      v-if="modal.visible"
+      v-show="modal.visible"
+      @click="closeModal"
     >
-      <div class="card bg-white w-100 rounded-3">
-        <div class="header p-3">
-          <h3>{{ modal }}</h3>
+      <Transition>
+        <div
+          class="card bg-white w-100 rounded-3"
+          v-if="modal.visible"
+          @click.stop
+        >
+          <div class="header p-3">
+            <h3 class="mb-0">{{ modal.title }}</h3>
+          </div>
+          <hr class="my-0" />
+          <div class="body p-3">
+            <p v-for="(item, index) in modal.description" :key="index">
+              {{ item }}
+            </p>
+          </div>
+          <div class="footer pb-3 px-3 d-flex justify-content-end">
+            <button @click="closeModal" class="btn btn-dark">Fechar</button>
+          </div>
         </div>
-        <hr />
-        <div class="body p-3">
-          <p v-for="(item, index) in modal.description" :key="index">
-            {{ item }}
-          </p>
-        </div>
-        <div class="footer pb-3 px-3 d-flex justify-content-end">
-          <button class="btn btn-dark">Fechar</button>
-        </div>
-      </div>
+      </Transition>
     </div>
-  </teleport>
+  </Teleport>
 </template>
 <style src="./styles.scss" scoped></style>
