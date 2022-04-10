06f4b49752ed31a4de3169182e32cff7d7e835ee
caioliveira277 <caio-henrique718@outlook.com>
2022-04-09 22:12:05 -0300

    adding flip card

--- src/views/more-details/MoreDetailsView.vue
+++ src/views/more-details/MoreDetailsView.vue
@@ -9,10 +9,17 @@ console.log(store);
 
 <template>
   <main class="container py-5 justify-content-center align-items-center d-flex">
-    <img
-      :src="currentPokemon.images.large"
-      :alt="`Imagem do pokemon ${currentPokemon.name}`"
-    />
+    <div class="flip-container">
+      <div class="flipper position-relative">
+        <div class="front">
+          <img
+            :src="currentPokemon.images.large"
+            :alt="`Imagem do pokemon ${currentPokemon.name}`"
+          />
+        </div>
+        <div class="back p-3 position-absolute top-0 w-100 h-100"></div>
+      </div>
+    </div>
   </main>
 </template>
 
